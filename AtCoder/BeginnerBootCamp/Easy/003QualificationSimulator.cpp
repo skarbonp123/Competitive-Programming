1b59fd3de993e59ca11ecd956a61f7c01f3c3cd8
Philippe <skarbonp123@gmail.com>
2025-02-27 14:25:40 +0100

    Problem 3 Solution

--- AtCoder/BeginnerBootCamp/Easy/003QualificationSimulator.cpp
+++ AtCoder/BeginnerBootCamp/Easy/003QualificationSimulator.cpp
@@ -3,7 +3,34 @@ using namespace std;
 
 int main() {
 
-    
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    string s;
+    cin >> s;
+
+    int count = 0;
+    int count_b = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'a') {
+            if (count < a + b) {
+                cout << "Yes" << "\n";
+                count++;
+            } else {
+                cout << "No" << "\n";
+            }
+        } else if (s[i] == 'b') {
+            if (count < a + b && count_b < b) {
+                cout << "Yes" << "\n";
+                count++;
+                count_b++;
+            } else {
+                cout << "No" << "\n";
+            }
+        } else {
+            cout << "No" << "\n";
+        }
+    }
 
     return 0;
 }
