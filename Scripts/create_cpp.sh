37b14acc71fa63062f40f743d35153531dcb0cb2
Philippe <skarbonp123@gmail.com>
2025-02-27 11:40:55 +0100

    Added a script folder to make a c++ file

---
+++ Scripts/create_cpp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo '#include <bits/stdc++.h>' > $1.cpp
+echo 'using namespace std;' >> $1.cpp
+echo '' >> $1.cpp
+echo ' int main() {' >> $1.cpp
+echo '' >> $1.cpp
+echo 'return 0;' >> $1.cpp
+echo '' >> $1.cpp
+echo '}' >> $1.cpp
+
