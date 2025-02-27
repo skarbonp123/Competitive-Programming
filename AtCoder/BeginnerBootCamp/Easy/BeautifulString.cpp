189e6032d0010a6678ff3076145efd8a6918058c
Philippe <skarbonp123@gmail.com>
2025-02-27 13:07:18 +0100

    BeautifulString solution

---
+++ AtCoder/BeginnerBootCamp/Easy/BeautifulString.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    string w;
+    cin >> w;
+
+    map<char, int> count;
+
+    for (char c : w) {
+        count[c]++;
+    }
+
+    for (auto pair : count) {
+        if (pair.second % 2 != 0) {
+            cout << "No";
+            return 0;
+        }
+    }
+
+    cout << "Yes";
+
+}
