10e45cfcebf92ac306758b1a1067a1241210eff0
Philippe <skarbonp123@gmail.com>
2025-02-27 19:58:55 +0100

    Sorry ass attempt at problem 2

--- AtCoder/BeginnerBootCamp/Medium/002IrreversibleOperation.cpp
+++ AtCoder/BeginnerBootCamp/Medium/002IrreversibleOperation.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main() {
     
-    
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    for (int j = 0; j < s.size()/2; j++) {
+        for (int i = s.size()-1; i >= 1; i--) {
+            if (s[i] == 'W' && s[i-1] == 'B') {
+                s[i] = 'B';
+                s[i-1] = 'W';
+                cnt++;
+            }
+        }
+    }
+        
+    cout << cnt;
+
     return 0;
 }
