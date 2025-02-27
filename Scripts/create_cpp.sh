6fb4eb5101b8e0443534b6954d7f19de844273e9
Philippe <skarbonp123@gmail.com>
2025-02-27 11:50:30 +0100

    Didnt push the new script for some reason

--- Scripts/create_cpp.sh
+++ Scripts/create_cpp.sh
@@ -2,9 +2,9 @@
 echo '#include <bits/stdc++.h>' > $1.cpp
 echo 'using namespace std;' >> $1.cpp
 echo '' >> $1.cpp
-echo ' int main() {' >> $1.cpp
-echo '' >> $1.cpp
-echo 'return 0;' >> $1.cpp
-echo '' >> $1.cpp
+echo 'int main() {' >> $1.cpp
+echo '    ' >> $1.cpp
+echo '    ' >> $1.cpp
+echo '    return 0;' >> $1.cpp
 echo '}' >> $1.cpp
 
