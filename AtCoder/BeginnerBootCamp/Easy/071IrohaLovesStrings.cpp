b78ef6b379f767ae4e3f6743d82eb76b773feb4a
Philippe <skarbonp123@gmail.com>
2025-02-27 14:09:00 +0100

    Problem 71 solution

---
+++ AtCoder/BeginnerBootCamp/Easy/071IrohaLovesStrings.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool compare(const string &a, const string &b) {
+    return a + b < b + a;
+}
+
+int main() {
+    
+    int N, L;
+    cin >> N >> L;
+    vector<string> S(N);
+    
+    for (int i=0; i<N; i++) {
+        cin >> S[i];
+    }
+
+    sort(S.begin(), S.end(), compare);
+
+    string result = "";
+    for (auto &s : S) {
+        result += s;
+    }
+
+    cout << result;
+    return 0;
+}
