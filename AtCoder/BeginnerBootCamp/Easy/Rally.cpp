17aa42cae3ffa81825fece561dc23d1559a7bf4e
Philippe <skarbonp123@gmail.com>
2025-02-27 13:04:17 +0100

    Rally solution

--- AtCoder/BeginnerBootCamp/Easy/Rally.cpp
+++ AtCoder/BeginnerBootCamp/Easy/Rally.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main() {
 
+    int n;
+    cin >> n;
+
+    vector<int> x(n);
+    float sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        x[i] = a;
+        sum += a;
+    }
+
+    sum = sum / n;
+    sum = round(sum);
+
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total += pow((x[i] - sum), 2);
+    }
+
+    cout << total;
     
     return 0;
 }
