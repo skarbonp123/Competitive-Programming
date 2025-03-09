0f2ff8e5cf880aa4a32f2aab06f8b9c4b6ad90d5
Philippe <skarbonp123@gmail.com>
2025-03-09 11:13:00 +0100

    Edit

--- Scripts/create_cpp.sh
+++ Scripts/create_cpp.sh
@@ -6,8 +6,8 @@ echo '#include <bits/stdc++.h>' > $1.cpp
 echo 'using namespace std;' >> $1.cpp
 echo '' >> $1.cpp
 echo 'int main() {' >> $1.cpp
-echo '  ios::sync_with_stdio(0);' >> $1.cpp
-echo '  cin.tie(0);' >> $1.cpp
+echo '    ios::sync_with_stdio(0);' >> $1.cpp
+echo '    cin.tie(0);' >> $1.cpp
 echo '    ' >> $1.cpp
 echo '    ' >> $1.cpp
 echo '    return 0;' >> $1.cpp
