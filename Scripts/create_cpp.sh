84d7563c305b1ee957121709eb85b47705bd77b5
Philippe <skarbonp123@gmail.com>
2025-03-09 11:06:15 +0100

    Updated cpp script

--- Scripts/create_cpp.sh
+++ Scripts/create_cpp.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo '/**' >> $1.cpp
+echo ' *    author: skarbonp' >> $1.cpp
+echo '**/' >> $1.cpp
 echo '#include <bits/stdc++.h>' > $1.cpp
 echo 'using namespace std;' >> $1.cpp
 echo '' >> $1.cpp
 echo 'int main() {' >> $1.cpp
+echo '  ios::sync_with_stdio(0);' >> $1.cpp
+echo '  cin.tie(0);' >> $1.cpp
 echo '    ' >> $1.cpp
 echo '    ' >> $1.cpp
 echo '    return 0;' >> $1.cpp
