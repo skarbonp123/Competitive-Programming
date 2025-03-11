b56b195d17709a10db8581f8baf2bd4f9f5aa17a
Philippe <skarbonp123@gmail.com>
2025-03-11 12:35:59 +0100

    Problem 13 Solution

--- AtCoder/BeginnerBootCamp/Easy/013CookieExchanges.cpp
+++ AtCoder/BeginnerBootCamp/Easy/013CookieExchanges.cpp
@@ -8,6 +8,29 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    int A, B, C; cin >> A >> B >> C;
+    int count = 0;
+
+    if (A==B && B==C) {
+        if (A % 2 == 0) {
+            cout << "-1";  
+            return 0;
+    }
+
+    while (A % 2 == 0 && B % 2 == 0 && C % 2 == 0) {
+        int tA,tB,tC;
+        tA = (B / 2) + (C / 2);
+        tB = (A / 2) + (C / 2);
+        tC = (A / 2) + (B / 2);
+        
+        A = tA;
+        B = tB;
+        C = tC;
+        count++;
+    }
+
+    cout << count << '\n';
     
     return 0;
 }
+ 
