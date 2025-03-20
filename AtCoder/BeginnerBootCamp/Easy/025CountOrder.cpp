da4cc9e9b13429e751b5b5b97ea30b4ee9867266
Philippe <skarbonp123@gmail.com>
2025-03-20 12:40:38 +0100

    Skipped Problem 25

---
+++ AtCoder/BeginnerBootCamp/Easy/025CountOrder.cpp
@@ -0,0 +1,15 @@
+/**
+ *    author: skarbonp
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N; cin>>N;
+    // n * n - 1 * n - 2
+    
+    return 0;
+}
