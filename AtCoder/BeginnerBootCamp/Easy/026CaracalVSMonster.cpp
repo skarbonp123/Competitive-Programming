1568c6da60da83190abef85c19360ddd7911279e
Philippe <skarbonp123@gmail.com>
2025-03-20 13:48:29 +0100

    Problem 26 Solution

--- AtCoder/BeginnerBootCamp/Easy/026CaracalVSMonster.cpp
+++ AtCoder/BeginnerBootCamp/Easy/026CaracalVSMonster.cpp
@@ -4,11 +4,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long monster_divide(long long H) {
+    if (H == 1) {
+        return 1;
+    } else if (H > 1) {
+        return 2 * monster_divide(H/2) + 1;
+    } else {
+        return 0;
+    }
+
+
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    
-    
-    return 0;
+    long long H; cin >> H;
+
+    cout << monster_divide(H) << '\n';
 }
+
