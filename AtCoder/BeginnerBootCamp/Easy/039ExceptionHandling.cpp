f4930667a24f743352c29d1a02e5efcea4460d41
Philippe <skarbonp123@gmail.com>
2025-03-28 16:44:27 +0100

    Problem 39 Skipped

---
+++ AtCoder/BeginnerBootCamp/Easy/039ExceptionHandling.cpp
@@ -0,0 +1,23 @@
+/**
+ *    author: skarbonp
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N; cin >> N ;
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        
+    }
+
+    return 0;
+}
