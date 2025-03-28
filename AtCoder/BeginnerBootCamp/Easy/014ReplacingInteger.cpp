34053c22b9946710d0f1c47697016360dfab0a38
Philippe <skarbonp123@gmail.com>
2025-03-28 17:29:47 +0100

    Problem 14 Solution

--- AtCoder/BeginnerBootCamp/Easy/014ReplacingInteger.cpp
+++ AtCoder/BeginnerBootCamp/Easy/014ReplacingInteger.cpp
@@ -8,7 +8,15 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
+    long long N, K; cin >> N >> K;
+
+    if (K == 1) { 
+        cout << 0;
+        return 0;
+    }
+
+    long long rem = N % K; 
+    cout << min (rem, K - rem) << '\n';
     
     return 0;
 }
