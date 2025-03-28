43a1a020b2af9cebacb0fe50be8bb0c04b92cb2c
Philippe <skarbonp123@gmail.com>
2025-03-28 14:22:25 +0100

    Script adjustment

--- Scripts/create_cpp.sh
+++ Scripts/create_cpp.sh
@@ -8,9 +8,9 @@ echo '' >> $1.cpp
 echo 'int main() {' >> $1.cpp
 echo '    ios::sync_with_stdio(0);' >> $1.cpp
 echo '    cin.tie(0);' >> $1.cpp
-echo '    ' >> $1.cpp
-echo '    ' >> $1.cpp
-echo '    ' >> $1.cpp
+echo '' >> $1.cpp
+echo '' >> $1.cpp
+echo '' >> $1.cpp
 echo '    return 0;' >> $1.cpp
 echo '}' >> $1.cpp
 
