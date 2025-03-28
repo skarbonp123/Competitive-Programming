878ee337147e64ce8111270f73602e164023ccba
Philippe <skarbonp123@gmail.com>
2025-03-28 17:46:37 +0100

    Problem 12 Solution

--- AtCoder/BeginnerBootCamp/Easy/012TravelingSalesman.cpp
+++ AtCoder/BeginnerBootCamp/Easy/012TravelingSalesman.cpp
@@ -10,7 +10,22 @@ int main() {
     
     int K, N; cin>>K>>N;
 
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int maximum = 0;
+
+    for (int i = 1; i < N; i++) {
+        maximum = max(maximum, A[i] - A[i - 1]);
+    }
     
+    maximum = max(maximum, K - A[N - 1] + A[0]);
+    
+
+    cout << K - maximum;
     
     return 0;
 }
