859f9de0ced846561d7412a5a788927e689951a6
Philippe <skarbonp123@gmail.com>
2025-03-30 17:23:12 +0200

    Problem 15 Solution

--- AtCoder/BeginnerBootCamp/Easy/015DivideTheProblems.cpp
+++ AtCoder/BeginnerBootCamp/Easy/015DivideTheProblems.cpp
@@ -8,6 +8,16 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
+    int N; cin >> N;
+    vector<int> D(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> D[i];
+    }
+
+    sort(D.begin(), D.end());
+
+    int half = D[N/2] - D[N/2 - 1];
+    cout << half;
     return 0;
 }
