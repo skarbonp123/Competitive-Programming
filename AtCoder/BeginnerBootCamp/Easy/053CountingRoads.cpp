f63c8ec5779ae2299052a47c245c517861c24a70
Philippe <skarbonp123@gmail.com>
2025-03-31 17:48:14 +0200

    Problem 53 Solution

--- AtCoder/BeginnerBootCamp/Easy/053CountingRoads.cpp
+++ AtCoder/BeginnerBootCamp/Easy/053CountingRoads.cpp
@@ -9,7 +9,19 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
+    int N, M; cin >> N >> M;
+
+    vector<int> C(N, 0);
+
+    for (int i = 0; i < M; i++) {
+        int a, b; cin >> a >> b;
+        C[a-1]++;
+        C[b-1]++;
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << C[i] << '\n';
+    }
     
     return 0;
 }
