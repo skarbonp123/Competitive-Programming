e059bc4a1f3fc893ed391a4085a223bf834b8176
Philippe <skarbonp123@gmail.com>
2025-04-02 20:08:54 +0200

    Problem 62 Solution

--- AtCoder/BeginnerBootCamp/Easy/062Thin.cpp
+++ AtCoder/BeginnerBootCamp/Easy/062Thin.cpp
@@ -9,27 +9,21 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int N; cin >> N;
-    vector<int> A(N);
-
-    bool ok = false;
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-        if (A[i] == 1) ok = true;
-    }
-    
-    
-    if (ok) {
-
-        int cnt = 1;
-        for (int i = 0; i < N; i++) {
-            if (A[i] == cnt) cnt++;
+    int H, W; cin >> H >> W;
+    vector<char> C(W);
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            cin >> C[j];
         }
-        cout << N - cnt + 1 << '\n';
-    } else {
-        cout << "-1\n";
-    }
-    
+        for (int j = 0; j < W; j++) {
+            cout << C[j];
+        }
+        cout << '\n';
+        for (int j = 0; j < W; j++) {
+            cout << C[j];
+        }
+        cout << '\n';
+        }
 
     return 0;
 }
