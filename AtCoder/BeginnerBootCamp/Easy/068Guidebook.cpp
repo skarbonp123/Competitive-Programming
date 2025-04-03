66a5e7141c077bee91f81941a0c60681f5da8f01
Philippe <skarbonp123@gmail.com>
2025-04-03 16:57:19 +0200

    Problem 68 Solution

--- AtCoder/BeginnerBootCamp/Easy/068Guidebook.cpp
+++ AtCoder/BeginnerBootCamp/Easy/068Guidebook.cpp
@@ -9,7 +9,20 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    int N; cin >> N;
+
+    map<pair<string, int> , int> S;
     
-    
+    for (int i = 0; i < N; i++) {
+        string s; cin >> s;
+        int p; cin >> p;
+        S[{s,100-p}] = i+1;
+    }
+
+    for (auto pair : S) {
+        cout << pair.second << '\n';
+    }
+
     return 0;
 }
+ 
