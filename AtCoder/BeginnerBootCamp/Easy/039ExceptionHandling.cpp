9498a8ecaf348f993a184c3695a6512d47c37099
Philippe <skarbonp123@gmail.com>
2025-09-03 20:24:48 +0200

    Problem 39 Solution

--- AtCoder/BeginnerBootCamp/Easy/039ExceptionHandling.cpp
+++ AtCoder/BeginnerBootCamp/Easy/039ExceptionHandling.cpp
@@ -9,15 +9,31 @@ int main() {
     cin.tie(0);
     
     int N; cin >> N ;
+    
     vector<int> A(N);
+    for (int &a : A) cin >> a;
+    
+    auto it = max_element(A.begin(), A.end());
+    int max1 = *it;
+    int max_idx = it - A.begin();
+    int cnt = count(A.begin(), A.end(), max1);
 
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
-
-    for (int i = 0; i < N; i++) {
+    if (cnt > 1) {
+        for (int i = 0; i < N; i++) {
+            cout << max1 << '\n';
+        }
+    } else {
         
-    }
+        int max2 = INT_MIN;
+        for (int i = 0; i < N; i++) { 
+            if (i == max_idx) continue;
+            max2 = max(max2, A[i]);
+        }
 
+        for (int i = 0; i < N; i++) {
+            if (i == max_idx) cout << max2 << '\n';
+            else              cout << max1 << '\n';
+        }
+    }
     return 0;
 }
