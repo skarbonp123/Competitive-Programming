c13315e816b41c4fe46190fd0630864bfb817e8f
Philippe <skarbonp123@gmail.com>
2025-09-03 20:23:56 +0200

    Problem 36 Solution

--- AtCoder/BeginnerBootCamp/Easy/036Prison.cpp
+++ AtCoder/BeginnerBootCamp/Easy/036Prison.cpp
@@ -8,7 +8,17 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> L(M), R(M);
+    for (int i = 0; i < M; i++) {
+        cin >> L[i] >> R[i];
+    }
+
+    int Lmax = *max_element(L.begin(), L.end());
+    int Rmin = *min_element(R.begin(), R.end());
     
-    
-    return 0;
+    cout << max(0, Rmin - Lmax + 1) << '\n';
+
 }
