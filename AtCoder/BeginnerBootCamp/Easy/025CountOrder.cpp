212764da14e1f103714f1e6ab3ddd673e28079a9
Philippe <skarbonp123@gmail.com>
2025-09-03 19:11:14 +0200

    Problem 25 Solution

--- AtCoder/BeginnerBootCamp/Easy/025CountOrder.cpp
+++ AtCoder/BeginnerBootCamp/Easy/025CountOrder.cpp
@@ -9,7 +9,25 @@ int main() {
     cin.tie(0);
     
     int N; cin>>N;
-    // n * n - 1 * n - 2
+    vector<int> P(N), Q(N);
+
+    for (int &p: P) cin >> p;
+    for (int &q: Q) cin >> q;
+
+    vector<int> base(N); 
+    iota(base.begin(), base.end(), 1);
+
+    int idx = 1;
+    int a = 0, b = 0;
+
+    do {
+        if (P == base) a = idx;
+        if (Q == base) b = idx;
+        if (a && b) break; //early exit
+        idx++;
+    } while (next_permutation(base.begin(), base.end()));
+
+    cout << abs(a - b) << '\n';
     
     return 0;
 }
