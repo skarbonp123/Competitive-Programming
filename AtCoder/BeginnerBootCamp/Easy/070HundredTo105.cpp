12c3f3c961f6c532d2408fb1375f8b4a538074ad
Philippe <skarbonp123@gmail.com>
2025-09-09 23:33:30 +0200

    Problem 70 Solution

--- AtCoder/BeginnerBootCamp/Easy/070HundredTo105.cpp
+++ AtCoder/BeginnerBootCamp/Easy/070HundredTo105.cpp
@@ -9,7 +9,17 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    
-    
+    int X;
+    cin >> X;
+
+    for (int i = 1; i <= 1000; ++i) {
+        int min_total = 100 * i;
+        int max_total = 105 * i;
+        if (X >= min_total && X <= max_total) {
+            cout << "1\n";
+            return 0;
+        }
+    }
+    cout << "0\n";
     return 0;
 }
