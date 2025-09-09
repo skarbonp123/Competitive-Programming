0473c6f0f9b949c10e4e2bee774538e96a37bc69
Philippe <skarbonp123@gmail.com>
2025-09-09 23:14:25 +0200

    Problem 59 Solution

--- AtCoder/BeginnerBootCamp/Easy/059AcCepted.cpp
+++ AtCoder/BeginnerBootCamp/Easy/059AcCepted.cpp
@@ -11,45 +11,34 @@ int main() {
     
     string s; cin >> s;
 
-    if (s[0] == 'A') {
-        if (islower(s[1])) {
-            string w = s.substr(2, s.length() - 3);
-            int tot_c = 0;
-            for (char c : w) {
-                if (isupper(c)) {
-                    if (c == 'C') {
-                        tot_c++;
-                        if (tot_c > 1) {
-                            cout << "WA\n";
-                            return 0;
-                        } else {
-                            if (islower(s[s.length()-1])) {
-                                cout << "AC\n";
-                                return 0;
-                            } else {
-                                cout << "WA\n";
-                                return 0;
-                            }
-                        }
-                    } else {
-                        cout << "WA\n";
-                        return 0;
-                    }
-                } else {
-                    if (tot_c == 0) {
-                        cout << "WA\n";
-                        return 0;
-                    }
+    string begin = s.substr(0, 1);
+    string mid = s.substr(2, s.size() - 3);
+    
+
+    int index = -1;
+    int count = 0;
+    for (int i = 0; i < mid.size(); i++) {
+        if (mid[i] == 'C') {
+            count++;
+            index = i + 2;
+        }
+    }
+
+    if (begin == "A" && count == 1) {
+        for (int i = 0; i < s.size(); i++) {
+            if (i == 0 || i == index) {
+                continue;
+            } else {
+                if (isupper(s[i])) {
+                    cout << "WA\n";
+                    return 0;
                 }
             }
-        } else {
-            cout << "WA\n";
-            return 0;
         }
+        cout << "AC\n";
     } else {
         cout << "WA\n";
     }
-
     
     return 0;
 }
